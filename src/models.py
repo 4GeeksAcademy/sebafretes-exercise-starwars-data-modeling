d8ca86c6ca04c43bc2a3a2907e317db6d50ef920
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-03-26 18:49:26 +0000

    models and diagram  done

--- src/models.py
+++ src/models.py
@@ -7,26 +7,39 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(200), nullable=False)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200), nullable=False)
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    planet = relationship(Planet)
+
+    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+    character = relationship(Character)
 
     def to_dict(self):
         return {}
+    
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
